a8c18d50845ce1c137bf66b472c725a052939ccb
yaasinslami <yassinx4002@gmail.com>
2024-12-26 14:00:43 +0100

    update 0x06

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,19 +1,25 @@
 #!/usr/bin/node
 const util = require('util');
 const request = util.promisify(require('request'));
+
 const filmID = process.argv[2];
 
-async function starwarsCharacters (filmId) {
-  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
-  let response = await (await request(endpoint)).body;
-  response = JSON.parse(response);
-  const characters = response.characters;
+async function starwarsCharacters(filmId) {
+  try {
+    const endpoint = `https://swapi-api.hbtn.io/api/films/${filmId}`;
+    const response = await request(endpoint);
+    const film = JSON.parse(response.body);
 
-  for (let i = 0; i < characters.length; i++) {
-    const urlCharacter = characters[i];
-    let character = await (await request(urlCharacter)).body;
-    character = JSON.parse(character);
-    console.log(character.name);
+    const characterPromises = film.characters.map(async (url) => {
+      const charResponse = await request(url);
+      const character = JSON.parse(charResponse.body);
+      return character.name;
+    });
+
+    const characters = await Promise.all(characterPromises);
+    characters.forEach((name) => console.log(name));
+  } catch (error) {
+    console.error('Error:', error.message);
   }
 }
 
